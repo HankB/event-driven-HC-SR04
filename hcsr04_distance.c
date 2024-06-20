4a8f76efc98c531f935cddeed3516b9951dbf20f
Hank Barta <hbarta@gmail.com>
2024-06-20 10:28:44 -0500

    progress toward working code

--- hcsr04_distance.c
+++ hcsr04_distance.c
@@ -2,20 +2,21 @@
 Measure distance using an HC-SR04 sensor using Interrupts.
 
 Build:
-    gcc -Wall -o hcsr04_distance hcsr04_distance.c  -lwiringPi
+    gcc -Wall -o hcsr04_distance hcsr04_distance.c  -l gpiod
 */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
-#include <wiringPi.h>
+#include <gpiod.h>
 
 static void send_pulse(int gpio);
 static float read_distance(void);
-void handle(void);
+int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused);
 
-static const int echo = 23; // GPIO # for echo input
+static const int echo = 23;                     // GPIO # for echo input
+static const struct timespec timeout = {10, 0}; // run for 10s and exit
 
 int main(int argc, char **argv)
 {
@@ -24,16 +25,28 @@ int main(int argc, char **argv)
 
     // printf("clocks/sec %ld\n", CLOCKS_PER_SEC);
     //  working with clocks/sec 1,000,000 on RpiOS 32 bit, Pi 3B
+    printf("init trigger\n");
+    int rc = gpiod_ctxless_set_value("/dev/gpiochip0",
+                                     trigger, 0, false,
+                                     "consumer", 0, 0);
+    if (rc < 0)
+    {
+        perror("gpiod_ctxless_get_value");
+        return 1;
+    }
 
-    // initialize GPIO
-    wiringPiSetupGpio();
-    pinMode(trigger, OUTPUT);   // configure trigger for output
-    digitalWrite(trigger, LOW); // initialize to low
+    send_pulse(trigger); // send the trigger pulse
+    printf("registering event\n");
+    rc = gpiod_ctxless_event_monitor("/dev/gpiochip0",
+                                     GPIOD_CTXLESS_EVENT_BOTH_EDGES,
+                                     echo, false, "consumer",
+                                     &timeout, 0, event_cb, 0);
+    printf("event registered\n");
+    if (rc < 0)
+        perror("gpiod_ctxless_get_value");
 
-    pinMode(echo, INPUT);                      // configure echo for input
-    digitalWrite(echo, LOW);                   // initialize to low
-    wiringPiISR(echo, INT_EDGE_BOTH, &handle); // bind ISR
-
+    printf("Hello World, rc is %d\n", rc);
+    /*
     while (1)
     {
         send_pulse(trigger); // send the trigger pulse
@@ -41,78 +54,61 @@ int main(int argc, char **argv)
         distance = read_distance();
         printf("distance %f\n", distance);
     }
+    */
 }
-
-static volatile clock_t start_echo; // time echo pulse starts
-static volatile clock_t end_echo;   // time return pulse starts
-
-typedef enum
-{
-    idle = 0,
-    waiting_hi = 1,
-    wait_lo = 2,
-} int_state;
-
-static volatile int_state state = idle;
-static volatile int int_count[3];
-
-#define timestamp_count 4
-static clock_t timestamp[timestamp_count];
 
 /*
  * At present send_pulse() not only sends the pulse but also polls for the result. for the
  */
 static void send_pulse(int gpio)
 {
-    timestamp[0] = clock();
-    state = waiting_hi;
-    digitalWrite(gpio, HIGH); // start trigger pulse
-    usleep(10);               // 10 microsecond (minimim) sleep
-    digitalWrite(gpio, LOW);  // finish trigger pulse
-    timestamp[1] = clock();
+    printf("sending pulse\n");
+    int rc = gpiod_ctxless_set_value("/dev/gpiochip0",
+                                     gpio, 1, false,
+                                     "consumer", 0, 0);
+    if (rc < 0)
+    {
+        perror("gpiod_ctxless_get_value");
+        return;
+    }
+    usleep(10); // 10 microsecond (minimim) sleep
+    rc = gpiod_ctxless_set_value("/dev/gpiochip0",
+                                 gpio, 0, false,
+                                 "consumer", 0, 0);
+    if (rc < 0)
+    {
+        perror("gpiod_ctxless_get_value");
+    }
 }
 
 static float read_distance(void)
 {
     // 39.5 empirically determined by taking readings at 6", 12" and 18"
     // to convert the readings to inches.
+    /*
     float distance = ((float)(end_echo - start_echo)) / 39.5;
     printf("%d %d %d ", int_count[0], int_count[1], int_count[2]);
     return distance;
+    */
+    printf("billions and billions ...\n");
+    return 9999999.0;
 }
 
-void handle(void)
+int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused)
 {
-    clock_t isr_entry = clock();        // capture time of ISR
-    int_count[state]++;                 // count ISR
-    int gpio_state = digitalRead(echo); // read the GPIO value
+    static struct timespec start = {0.0};
 
-    printf("state%d\n", state);
+    printf("i: %d, j:%d at %ld.%9.9ld\n", i, j, ts->tv_sec, ts->tv_nsec);
 
-    switch (state)
+    if (GPIOD_CTXLESS_EVENT_CB_RISING_EDGE == i)
     {
-    case idle:
-        break;
-
-    case waiting_hi:
-        if (gpio_state)
-        {
-            start_echo = isr_entry;
-            state = wait_lo;
-        }
-        break;
-
-    case wait_lo:
-        if (!gpio_state)
-        {
-            end_echo = isr_entry;
-            state = idle;
-            printf("delta: %ld\n", end_echo - start_echo);
-        }
-        break;
-
-    default:
-        printf("state: %d\n", state);
-        exit(-1);
+        start = *ts;
     }
+    else if (GPIOD_CTXLESS_EVENT_CB_FALLING_EDGE == i)
+    {
+        long nsec = ts->tv_nsec - start.tv_nsec;
+        printf( "delta-T:%ld\n", nsec);
+    }
+
+    return 0;
 }
