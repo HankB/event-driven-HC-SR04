bce7b8f91a326cf968748b9c4989cc33ab2c0b5d
Hank Barta <hbarta@gmail.com>
2024-06-22 11:22:05 -0500

    cleanup

--- hcsr04_distance.c
+++ hcsr04_distance.c
@@ -15,50 +15,37 @@ static void send_pulse(int gpio);
 static float read_distance(void);
 int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused);
 
-static const int echo = 23;                     // GPIO # for echo input
-static const struct timespec timeout = {10, 0}; // run for 10s and exit
+static const int echo = 23;                    // GPIO # for echo input
+static const struct timespec timeout = {1, 0}; // run for 1s and exit
 
 int main(int argc, char **argv)
 {
     float distance;                // distance we measure
     static const int trigger = 24; // GPIO # for trigger output
 
-    // printf("clocks/sec %ld\n", CLOCKS_PER_SEC);
-    //  working with clocks/sec 1,000,000 on RpiOS 32 bit, Pi 3B
-    printf("init trigger\n");
+    // init the ytihhrt output
     int rc = gpiod_ctxless_set_value("/dev/gpiochip0",
                                      trigger, 0, false,
                                      "consumer", 0, 0);
     if (rc < 0)
     {
-        perror("gpiod_ctxless_get_value");
+        perror("gpiod_ctxless_set_value");
         return 1;
     }
 
     send_pulse(trigger); // send the trigger pulse
-    printf("registering event\n");
     rc = gpiod_ctxless_event_monitor("/dev/gpiochip0",
                                      GPIOD_CTXLESS_EVENT_BOTH_EDGES,
                                      echo, false, "consumer",
                                      &timeout, 0, event_cb, 0);
-    printf("event registered\n");
     if (rc < 0)
-        perror("gpiod_ctxless_get_value");
-
-    printf("Hello World, rc is %d\n", rc);
-    /*
-    while (1)
-    {
-        send_pulse(trigger); // send the trigger pulse
-        usleep(500000);      // wait half second (needed?)
-        distance = read_distance();
-        printf("distance %f\n", distance);
-    }
-    */
+        perror("gpiod_ctxless_event_monitor");
+    distance = read_distance();
+    printf("Hello World, rc is %d, distance %f\n", rc, distance);
 }
 
 /*
- * At present send_pulse() not only sends the pulse but also polls for the result. for the
+ * send pulse to initiate measurement
  */
 static void send_pulse(int gpio)
 {
@@ -68,7 +55,7 @@ static void send_pulse(int gpio)
                                      "consumer", 0, 0);
     if (rc < 0)
     {
-        perror("gpiod_ctxless_get_value");
+        perror("gpiod_ctxless_set_value");
         return;
     }
     usleep(10); // 10 microsecond (minimim) sleep
@@ -77,26 +64,26 @@ static void send_pulse(int gpio)
                                  "consumer", 0, 0);
     if (rc < 0)
     {
-        perror("gpiod_ctxless_get_value");
+        perror("gpiod_ctxless_set_value");
     }
 }
 
+static struct timespec start = {0.0};  // start of echo pulse
+static struct timespec finish = {0.0}; // end of echo pulse
+
+// read_distance() will calculate the distance based on time stamps collected
+// by event_cb() 
 static float read_distance(void)
 {
-    // 39.5 empirically determined by taking readings at 6", 12" and 18"
-    // to convert the readings to inches.
-    /*
-    float distance = ((float)(end_echo - start_echo)) / 39.5;
-    printf("%d %d %d ", int_count[0], int_count[1], int_count[2]);
+    float pulse_width = (float)(finish.tv_nsec - start.tv_nsec) / 1000000000;
+    // TODO handle rollover of tv_sec between readings
+    float distance = pulse_width*1100/2.0;
     return distance;
-    */
-    printf("billions and billions ...\n");
-    return 9999999.0;
 }
 
+// callback for GPIO transitions on the echo pin
 int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused)
 {
-    static struct timespec start = {0.0};
 
     printf("i: %d, j:%d at %ld.%9.9ld\n", i, j, ts->tv_sec, ts->tv_nsec);
 
@@ -106,8 +93,12 @@ int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused)
     }
     else if (GPIOD_CTXLESS_EVENT_CB_FALLING_EDGE == i)
     {
-        long nsec = ts->tv_nsec - start.tv_nsec;
-        printf( "delta-T:%ld\n", nsec);
+        finish = *ts;
+        return 1; // will result in return of gpiod_ctxless_event_monitor()
+    }
+    else // timeout on monitor (set by struct timespec timeout)
+    {
+        return 1;
     }
 
     return 0;
