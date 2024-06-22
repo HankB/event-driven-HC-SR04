3bde3f0ef0ad4672246c7488eced019150ce3b0d
Hank Barta <hbarta@gmail.com>
2024-06-22 16:58:31 -0500

    cleanup

--- hcsr04_distance.c
+++ hcsr04_distance.c
@@ -21,9 +21,6 @@ static int send_pulse(struct gpiod_line *);
 static float read_distance(void);
 int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused);
 
-// static const int echo = 23;                           // GPIO # for echo input
-// static const int trigger = 24;                        // GPIO # for trigger output
-//  static const struct timespec timeout = {1, 0};        // monitor events for up to 1s and exit
 static const char *cnsmr = "HC-SR04";                 // declare a consumer to use for calls
 static const char *GPIO_chip_name = "/dev/gpiochip0"; // name of trigger GPIO (output)
 static const char *trigger_name = "GPIO24";           // name of trigger GPIO (output)
@@ -96,30 +93,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
-#if 0
-    const struct gpiod_line_request_config read_config =
-        {cnsmr,
-         GPIOD_LINE_REQUEST_DIRECTION_INPUT,
-         0};
 
-    rc = gpiod_line_request(echo_line,
-                            &read_config,
-                            0);
-    if (rc < 0)
-    {
-        perror("               gpiod_line_request(echo_line)");
-        gpiod_line_release(echo_line);
-        gpiod_chip_close(chip);
-    }
-
-    printf("===== line configured for read \n");
-    report_line_attributes(echo_line, cnsmr);
-    printf("============\n\n");
-
-    sleep(1); // allow input, output to settle
-#endif
-
-    ////////////////////////////////////////////////
     rc = gpiod_line_request_both_edges_events(echo_line, cnsmr);
     printf("%d = gpiod_line_request_both_edges_events()\n", rc);
     if (0 > rc)
@@ -192,12 +166,6 @@ int main(int argc, char **argv)
     }
     ////////////////////////////////////////////////
 
-#if 0
-
-
-    distance = read_distance();
-    printf("Hello World, rc is %d, distance %f\n", rc, distance);
-#endif
     gpiod_line_release(trigger_line);
     gpiod_line_release(echo_line);
     gpiod_chip_close(chip);
@@ -231,56 +199,3 @@ static float read_distance(void)
     float distance = pulse_width * 1100 / 2.0;
     return distance;
 }
-
-#if 0
-/*
- * Callback for send_pulse() to delay 10ns.
- */
-static void send_pulse_cb(void *unused)
-{
-    usleep(10); // 10 microsecond (minimim) sleep
-}
-/*
- * send pulse to initiate measurement. Callback will delay 10ns
- * And at the completion of gpiod_ctxless_set_value() the pulse will
- * turn off. I think.
- */
-static void send_pulse(int gpio)
-{
-    printf("sending pulse\n");
-    int rc = gpiod_ctxless_set_value("/dev/gpiochip0",
-                                     gpio, 1, false,
-                                     "consumer", send_pulse_cb, 0);
-    if (rc < 0)
-    {
-        perror("send_pulse(): gpiod_ctxless_set_value");
-    }
-}
-
-static struct timespec start = {0.0};  // start of echo pulse
-static struct timespec finish = {0.0}; // end of echo pulse
-
-
-// callback for GPIO transitions on the echo pin
-int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused)
-{
-
-    printf("i: %d, j:%d at %ld.%9.9ld\n", i, j, ts->tv_sec, ts->tv_nsec);
-
-    if (GPIOD_CTXLESS_EVENT_CB_RISING_EDGE == i)
-    {
-        start = *ts;
-    }
-    else if (GPIOD_CTXLESS_EVENT_CB_FALLING_EDGE == i)
-    {
-        finish = *ts;
-        return 1; // will result in return of gpiod_ctxless_event_monitor()
-    }
-    else // timeout on monitor (set by struct timespec timeout)
-    {
-        return 1;
-    }
-
-    return 0;
-}
-#endif
